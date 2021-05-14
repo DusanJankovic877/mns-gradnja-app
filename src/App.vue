46fd174c53aa8e36b4bc7c02de17c608c994638c
Dusan <dusanjankovic877@gmail.com>
2021-05-14 16:49:59 +0200

    chaned transition to 1sec, added smaller version of logo, cnaged tab logo to small logo

--- src/App.vue
+++ src/App.vue
@@ -99,7 +99,7 @@ button{
 }
 
 .fade-in-enter-active {
-    transition: opacity .7s ease-in-out, transform .7s ease;
+    transition: opacity 1s ease-in-out, transform 1s ease;
     transition-delay: 1s;
 }
 
